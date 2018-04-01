20d66bc173e98c9b828282a2f722d81cb2b565d5
Marvin <marvinshawn9333@gmail.com>
2018-04-01 22:30:23 +0800

    change cookie

--- qiushibaike/settings.py
+++ qiushibaike/settings.py
@@ -68,7 +68,7 @@ DEFAULT_REQUEST_HEADERS = {
   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
   'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
   'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36',
-  'Cookie':'_xsrf=2|fcc8798e|1fed86277b3dba7290ed47443048f897|1513519767; _ga=GA1.2.1115992353.1513519769; ADEZ_BLOCK_SLOT=FUCKIE; ADEZ_ST=FUCKIE; ADEZ_PVC=1026761-1-jbbbqo0q; _qqq_uuid_="2|1:0|10:1519272590|10:_qqq_uuid_|56:NmVlN2UwYjhjNzZiOGQ5NDBlY2FiM2Q5YzEwN2UyZTdiYzlmYzFlYg==|9005734482c01ac34f5c49a6622deec83a6308910783e18073a6af80e7011581"; __cur_art_index=4904; Hm_lvt_2670efbdd59c7e3ed3749b458cafaa37=1521369411,1521376213,1521376230,1521460843; _gid=GA1.2.352018835.1521720677; Hm_lpvt_2670efbdd59c7e3ed3749b458cafaa37=1521723564'
+  'Cookie':'_xsrf=2|fcc8798e|1fed86277b3dba7290ed47443048f897|1513519767; _ga=GA1.2.1115992353.1513519769; ADEZ_BLOCK_SLOT=FUCKIE; ADEZ_ST=FUCKIE; ADEZ_PVC=1026761-1-jbbbqo0q; __cur_art_index=4904; Hm_lvt_2670efbdd59c7e3ed3749b458cafaa37=1521369411,1521376213,1521376230,1521460843; _gid=GA1.2.179938906.1522591898; _qqq_uuid_="2|1:0|10:1522591906|10:_qqq_uuid_|56:NTQyYzk2MjRlYjhiNTk2YTUxMTI2YjkzN2MzODYwODQzZjEzYzVmZQ==|6eab6821dfce6f6e88ec9d760b621d1a3fa7fd6a1dd2a5be76a215a57fb55ffb"; Hm_lpvt_2670efbdd59c7e3ed3749b458cafaa37=1522592860; _gat=1'
 }
 
 # Enable or disable spider middlewares
